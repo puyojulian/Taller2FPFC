94b4d1c1401ac70117e285fe52d3f7e1103c74c0
Julian Puyo <112900679+puyojulian@users.noreply.github.com>
2023-09-20 19:52:14 -0500

    adelantos sobre chip not

--- src/main/scala/Circuitos/package.scala
+++ src/main/scala/Circuitos/package.scala
@@ -1,3 +1,9 @@
 package object Circuitos {
+  type Chip = List[Int] => List[Int]
+
+  def crearChipUnario(funcion: Int => Int): Chip = {
+    def chip(lista: List[Int]) = List(funcion(lista.head))
+    chip
+  }
 
 }
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -1,1 +1,5 @@
 import Circuitos._
+
+val chip_not = crearChipUnario((x:Int) => (1-x))
+chip_not(List(1))
+chip_not(List(0))
